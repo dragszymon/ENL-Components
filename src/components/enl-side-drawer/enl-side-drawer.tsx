fb0d9b7689d7eb3f7d9db078b96a38b0e7e19b33
Szymon <szymon@enlighten.pl>
2019-06-18 12:36:37 +0200

    Stencil Init

---
+++ src/components/enl-side-drawer/enl-side-drawer.tsx
@@ -0,0 +1,50 @@
+import { Component, Prop, Method, Event, EventEmitter, h } from '@stencil/core';
+
+@Component({
+    tag: 'enl-side-drawer',
+    styleUrl: 'style.css',
+})
+export class EnlSideDrawer {
+    @Prop({ reflectToAttr: true, mutable: true }) opened: boolean;
+
+    @Event({ bubbles: false }) onOpen: EventEmitter;
+    @Event({ bubbles: false }) onClose: EventEmitter;
+    @Event({ bubbles: false }) onToggle: EventEmitter<boolean>;
+
+    @Method() open() {
+        this._open()
+    }
+    @Method() close() {
+        this._close()
+    }
+
+    _open(){
+        this.opened = true;
+        this.onOpen.emit()
+        this.onToggle.emit(true)
+    }
+
+    _close(){
+        this.opened = false;
+        this.onClose.emit()
+        this.onToggle.emit(false)
+    }
+    
+    render(){
+        return [
+            <div class="enl-side-drawer__drawer">
+                <div class="header">
+                    <slot name="header" />
+                </div>
+                <div class="content">
+                    <slot name="content" />
+                </div>
+            </div>,
+
+            <div 
+                onClick={this._close.bind(this)}
+                class="enl-side-drawer__backdrop">
+            </div>
+        ]
+    }
+}
